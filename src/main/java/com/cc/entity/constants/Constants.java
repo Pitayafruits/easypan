f12859e707d637e182aca7e070bf399a088917c4
“pitayafruit” <1037153356@qq.com>
2023-08-21 23:18:36 +0800

    feat：添加常量类的值

--- src/main/java/com/cc/entity/constants/Constants.java
+++ src/main/java/com/cc/entity/constants/Constants.java
@@ -8,6 +8,12 @@ public class Constants {
     //登录注册
     public static final String CHECK_CODE_KEY = "check_code_key";
 
-    //:邮箱验证码发送
+    //邮箱验证码发送
     public static final String CHECK_CODE_KEY_EMAIL = "check_code_key_email";
+
+    //数字验证码位数
+    public static final Integer LENGTH_5 = 5;
+
+    //状态码
+    public static final Integer ZERO = 0;
 }
