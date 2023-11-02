9f0034cef4400bbd1e9e181ebd7dd7b0b4099048
CC <112964687+Pitayafruits@users.noreply.github.com>
2023-11-02 23:15:33 +0800

    feat:添加获取用户头像的接口

--- src/main/java/com/cc/controller/ABaseController.java
+++ src/main/java/com/cc/controller/ABaseController.java
@@ -3,18 +3,64 @@ package com.cc.controller;
 import com.cc.entity.vo.ResponseVO;
 
 import com.cc.enums.ResponseCodeEnum;
+import com.cc.utils.StringTools;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
 
 public class ABaseController {
 
     protected static final String STATUC_SUCCESS = "success";
     protected static final String STATUC_ERROR = "error";
 
-    protected <T>ResponseVO getSuccessResponseVO(T t){
+    protected static final Logger logger = LoggerFactory.getLogger(ABaseController.class);
+
+    protected <T> ResponseVO getSuccessResponseVO(T t) {
         ResponseVO<T> responseVO = new ResponseVO<>();
         responseVO.setStatus(STATUC_SUCCESS);
         responseVO.setCode(ResponseCodeEnum.CODE_200.getCode());
         responseVO.setInfo(ResponseCodeEnum.CODE_200.getMsg());
         responseVO.setData(t);
         return responseVO;
+    }
+
+    protected void readFile(HttpServletResponse response, String filePath) {
+        if (!StringTools.pathIsOk(filePath)) {
+            return;
+        }
+        try {
+            OutputStream out = null;
+            FileInputStream in = null;
+            File file = new File(filePath);
+            if (!file.exists()) {
+                return;
+            }
+            in = new FileInputStream(file);
+            byte[] byteData = new byte[1024];
+            out = response.getOutputStream();
+            int len = 0;
+            while ((len = in.read(byteData)) != -1) {
+                out.write(byteData, 0, len);
+            }
+            out.flush();
+        } catch (IOException e) {
+            logger.error("读取文件异常", e);
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    logger.error("IO异常", e);
+                }
+            }
+        }
     }
 }
--- src/main/java/com/cc/controller/UserController.java
+++ src/main/java/com/cc/controller/UserController.java
@@ -3,6 +3,7 @@ package com.cc.controller;
 
 import com.cc.annotaion.GlobalInterceptor;
 import com.cc.annotaion.VerifyParam;
+import com.cc.config.AppConfig;
 import com.cc.entity.constants.Constants;
 import com.cc.entity.dto.SessionWebUserDto;
 import com.cc.entity.vo.ResponseVO;
@@ -17,9 +18,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.cc.utils.CreateImageCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 
 /**
@@ -29,11 +35,21 @@ import java.io.IOException;
 @RequestMapping("/user")
 public class UserController extends ABaseController {
 
+    private static final String CONTENT_TYPE = "Content-Type";
+
+    private static final String CONTENT_TYPE_VALUE = "application/json;charset=UTF-8";
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Resource
     private UserService userService;
 
     @Resource
     private EmailCodeService emailCodeService;
+
+    @Resource
+    private AppConfig appConfig;
+
 
     /**
      * 获取验证码
@@ -143,5 +159,46 @@ public class UserController extends ABaseController {
         }
     }
 
+    /**
+     * 获取头像
+     */
+    @GetMapping("/getAvatar/{userId}")
+    @GlobalInterceptor(checkParams = true)
+    public void getAvatar(HttpServletResponse response, @VerifyParam(required = true) @PathVariable("userId") String userId) {
+            //声明头像存储地址
+            String avatarFoldName = Constants.FILE_FOLDER_FILE + Constants.FILE_FOLDER_AVATAR_NAME;
+            File folder = new File(avatarFoldName);
+            if (!folder.exists()) {
+                folder.mkdirs();
+            }
+            String avatarPath = appConfig.getProjectFolder() + avatarFoldName + userId + Constants.AVATAR_SUFFIX;
+            File file = new File(avatarPath);
+            if (!file.exists()) {
+                if (!new File(appConfig.getProjectFolder() + avatarFoldName + Constants.AVATAR_DEFAULT).exists()) {
+                    printNoDefaultImage(response);
+                }
+                avatarPath = appConfig.getProjectFolder() + avatarFoldName + Constants.AVATAR_DEFAULT;
+            }
+            response.setContentType("image/jpg");
+            readFile(response, avatarPath);
+        }
+
+    /**
+     * 头像输出异常提示
+     */
+    private void printNoDefaultImage(HttpServletResponse response){
+        response.setHeader(CONTENT_TYPE, CONTENT_TYPE_VALUE);
+        response.setStatus(HttpStatus.OK.value());
+        PrintWriter writer = null;
+        try {
+            writer = response.getWriter();
+            writer.print("请在头像目录下放置默认头像default_avatar.jpg");
+            writer.close();
+        } catch (IOException e) {
+            logger.error("输出无默认图失败", e);
+        }finally {
+            writer.close();
+        }
+    }
 
 }
