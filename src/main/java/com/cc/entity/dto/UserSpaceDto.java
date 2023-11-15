bc10f24b2972109ac6de79945e1597dfcdb56d21
CC <112964687+Pitayafruits@users.noreply.github.com>
2023-11-15 22:24:06 +0800

    fix:用户空间dto添加序列化注解

--- src/main/java/com/cc/entity/dto/UserSpaceDto.java
+++ src/main/java/com/cc/entity/dto/UserSpaceDto.java
@@ -1,5 +1,6 @@
 package com.cc.entity.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -8,6 +9,7 @@ import java.io.Serializable;
  * 用户空间dto
  */
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserSpaceDto implements Serializable {
 
     private Long userSpace;
